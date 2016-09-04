a23e61133a340fc720ec0ccb4b24649402315c4e
B-raw <michaelbarry1989@hotmail.com>
2016-09-04 03:36:57 +0100

    finishing exercise 3

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
 #write your code here
+def echo(word)
+  "#{word}"
+end
+
+def shout(word)
+  "#{word.upcase}"
+end
+
+def repeat(word, number=2)
+  ((word + " ") * (number-1)) + word
+end
+
+def start_of_word(word, no_of_letters)
+  word[0,no_of_letters]
+end
+
+def first_word(word)
+  array = word.split(' ')
+  array[0]
+end
+
+def titleize(word)
+  array = word.split(' ')
+  count = 0
+  array.map! do |word|
+    exception_array = ["and", "over", "the"]
+    if exception_array.include?(word) && count != 0
+      count +=1
+      word
+    else
+      count +=1
+      word.capitalize
+    end
+  end
+  array.join(' ')
+end
