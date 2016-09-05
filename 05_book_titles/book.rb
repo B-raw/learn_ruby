7cdff78bb67c88644025bd45602bb2623339e41f
B-raw <michaelbarry1989@hotmail.com>
2016-09-05 16:07:11 +0100

    finished book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  attr_accessor:title
+
+  def title
+    articles = ["and", "in", "the", "of", "a", "an"]
+    count = 0
+    title_array = @title.split(" ").map! do |word|
+      if count == 0
+        count += 1
+        word.capitalize
+      elsif articles.include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+    title_array.join(" ")
+  end
 end
